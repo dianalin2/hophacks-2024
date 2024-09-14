689987cd218b7459deda37c88ea691ca82ed3db9
Phrygan <honga010807@gmail.com>
2024-09-14 14:30:33 -0400

    word families

---
+++ scripts/parse_word_families.py
@@ -0,0 +1,22 @@
+import pickle
+
+
+word_families = {}
+with open("data/word_families.txt", 'r') as file_object:
+    line = file_object.readline()
+    current_key = ""
+    while line:
+        if(not line[0].isspace()):
+            current_key = line.strip()
+            word_families[current_key] = []
+        else:
+            word_families[current_key].append(line.strip())
+        line = file_object.readline()
+
+#reorganize word families
+word_representative = {}
+for rep in word_families.keys():
+    for family_member in word_families[rep]:
+        word_representative[family_member] = rep
+
+
