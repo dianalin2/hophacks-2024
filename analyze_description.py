5195a93bd4e7e32c2b2a06a90288b54117b19e7a
Jessica Su <aes6qb@virginia.edu>
2024-09-14 09:19:43 -0400

    removed spaces from symptoms list

--- analyze_description.py
+++ analyze_description.py
@@ -3,15 +3,18 @@ import csv
 symptoms = []
 
 def collect_symptoms_data():
-    with open('symptom_severity.csv','r') as csv_file:
+    with open('data/symptom_severity.csv','r') as csv_file:
         reader =  csv.reader(csv_file)
 
         for row in reader:
             symptoms.append(row[0])
+            symptoms[-1] = symptoms[-1].replace("_"," ")
         csv_file.close()
-        
+
     symptoms.remove(symptoms[0])
     
+
+
 collect_symptoms_data()
 print(symptoms)
 
