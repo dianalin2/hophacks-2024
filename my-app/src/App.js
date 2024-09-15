a62273a32290e313723e759fc9ae8a42a05bdec2
Diana Lin <lindiana1206@gmail.com>
2024-09-15 08:42:57 -0400

    win

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,46 +2,57 @@ import './App.css';
 import Triage from './components/triage_dropdown';
 import PDFviewer from './components/pdfviewer';
 import PatientRow from './components/PatientRow';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Triage />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
 
-      </header>
-      </header>
-      <PDFviewer/> */}
-      <div className="MainContainer">
-        <p className="AppTitle">Patient Data</p>
-        <table className="InfoTable">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Number</th>
-              <th>Timestamp</th>
-              <th>Prediction</th>
-            </tr>
-          </thead>
-          <tbody>
-            <PatientRow patientID={123} name="Andrew Hong" number="217-281-1471" timestamp="9/14/24 9:38pm" linkToReport="Click me!"/>
-          </tbody>
-        </table>
+  async componentDidMount() {
+    const res = await fetch(`/api/userdata`);
+
+    const data = await res.json();
+
+    console.log(data);
+
+    this.setState({
+      data: data
+    });
+  }
+
+  render() {
+    const patientRows = this.state.data.map((patient, index) => {
+      return (
+        <PatientRow key={index} pName={patient.name} number={patient.number} timestamp={patient.timestamp} linkToReport={'Click to View Report'} patientID={patient.id} />
+      );
+    })
+
+    return (
+      <div className="App">
+        <div className="MainContainer">
+          <p className="AppTitle">Patient Data</p>
+          <table className="InfoTable">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Number</th>
+                <th>Timestamp</th>
+                <th></th>
+                <th>Prediction</th>
+              </tr>
+            </thead>
+            <tbody>
+              {patientRows}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- my-app/src/components/PatientRow.js
+++ my-app/src/components/PatientRow.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
-import pdf from "./document.pdf";
+import React, { Component } from 'react';
 
-function PatientRow ({ patientID, name, number, timestamp, linkToReport}) {
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{number}</td>
-            <td>{timestamp}</td>
-            <td><a href={pdf} target="_blank" rel="noreferrer">{linkToReport}</a></td>
-        </tr>
-    );
-};
+class PatientRow extends Component {
+    constructor() {
+        super();
+        this.startCall = this.startCall.bind(this);
+    }
+
+    startCall() {
+        fetch(`/api/patientpdf/${this.props.patientID}/startcall`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    render() {
+        console.log(this.pName)
+        return (
+            <tr>
+                <td>{this.props.pName}</td>
+                <td>{this.props.number}</td>
+                <td>{this.props.timestamp}</td>
+                <td><button onClick={this.startCall}>Start Pre-Call</button></td>
+                <td><a href={`/api/patientpdf/${this.props.patientID}`} target="_blank" rel="noreferrer">{this.props.linkToReport}</a></td>
+            </tr>
+        );
+    }
+}
 
 export default PatientRow;
